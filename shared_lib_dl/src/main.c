b8b52d591c49932409a13049dc3f18c045e0f00c
Andrey525 <ghost5run@yandex.ru>
2022-01-06 22:42:45 +0700

    Split into several libs, readdir and find lib.so

--- shared_lib_dl/src/main.c
+++ shared_lib_dl/src/main.c
@@ -4,8 +4,11 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
 
 #define MAX_DIGITS 5 + 1
+#define MAX_COUNT_PLUGINS 10
 
 int enter_number(int *rezult) {
     char str[MAX_DIGITS];
@@ -27,6 +30,36 @@ int enter_number(int *rezult) {
     return 0;
 }
 
+int read_plugins_dir(char **plugins) {
+    int count = 0;
+    int i;
+    for (i = 0; i < MAX_COUNT_PLUGINS; i++) {
+        plugins[i] = malloc(sizeof(char) * 256);
+    }
+    struct dirent *entry;
+    DIR *dir;
+    dir = opendir("./lib");
+    if (dir == NULL) {
+        perror("Read dir error");
+        return -1;
+    }
+    errno = 0;
+    i = 0;
+    while ((entry = readdir(dir)) != NULL) {
+        if (strcmp(".", entry->d_name) != 0 &&
+            strcmp("..", entry->d_name) != 0) {
+            plugins[i] = strcpy(plugins[i], entry->d_name);
+            count++;
+            i++;
+        }
+    }
+    if (errno && !entry) {
+        perror("Print dir error");
+    }
+    closedir(dir);
+    return count;
+}
+
 int main(int argc, char *argv[]) {
 	void *handle;
 	int (*arithmetic_operation)(int, int);
@@ -35,12 +68,8 @@ int main(int argc, char *argv[]) {
     int num1, num2, rezult;
     int flag;
     int operation;
-
-	handle = dlopen("./lib/libcalc.so", RTLD_LAZY);
-	if (!handle) {
-		fputs(dlerror(), stderr);
-        exit(1);
-	}
+    char *plugins[MAX_COUNT_PLUGINS];
+    int count;
 
     while(1) {
         printf("1 - Продолжить\n"
@@ -51,7 +80,7 @@ int main(int argc, char *argv[]) {
             printf("Введите номер действия: ");
             flag = enter_number(&operation);
             if (flag == 0 && (operation < 1 || operation > 2)) {
-                printf("Введен неверное действие!\n");
+                printf("Введено неверное действие!\n");
                 flag = 1;
             }    
         }
@@ -74,46 +103,42 @@ int main(int argc, char *argv[]) {
             flag = enter_number(&num2);
         }
 
-        printf("Выберите операцию:\n"
-            "1 - Сложение\n"
-            "2 - Вычитание\n"
-            "3 - Умножение\n"
-            "4 - Деление\n");
+        printf("Выберите операцию:\n");
+        count = read_plugins_dir(plugins);
+        for (int i = 0; i < count; i++) {
+            printf("%d - %s\n", i, plugins[i]);
+        }
 
         flag = 1;
         while (flag != 0) {
             printf("Введите номер операции: ");
             flag = enter_number(&operation);
-            if (flag == 0 && (operation < 1 || operation > 4)) {
+            if (flag == 0 && (operation < 0 || operation > count)) {
                 printf("Введен неверный номер операции!\n");
                 flag = 1;
             }    
         }
 
-        switch(operation) {
-        case 1:
-            arithmetic_operation = dlsym(handle, "sum");
-            break;
-        case 2:
-            arithmetic_operation = dlsym(handle, "subtraction");
-            break;
-        case 3:
-            arithmetic_operation = dlsym(handle, "multiplication");
-            break;
-        case 4:
-            arithmetic_operation = dlsym(handle, "division");
-            break;
-        default:
-            break;
+        char *string = malloc(sizeof(char) * 256);
+        string = strcpy(string, "./lib/");
+        string = strcat(string, plugins[operation]);
+        handle = dlopen(string, RTLD_LAZY);
+        if (!handle) {
+            fputs(dlerror(), stderr);
+            exit(1);
         }
+        char *string2 = malloc(sizeof(char) * 256);
+        string2 = strcpy(string2, plugins[operation]);
+        string2 = string2 + 3;
+        string2[strlen(string2) - 3] = '\0';
+        arithmetic_operation = dlsym(handle, string2);
         if ((error = dlerror()) != NULL)  {
             fprintf (stderr, "%s\n", error);
             exit(1);
         }
         rezult = arithmetic_operation(num1, num2);
-
         printf("Результат = %d\n", rezult);
+        dlclose(handle);
     }
-    dlclose(handle);
 	return 0;
 }
