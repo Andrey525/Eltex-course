4a61e1e980ab7947d3cfb1cf34d7b2d5a4dfe3b3
Andrey525 <ghost5run@yandex.ru>
2022-01-12 20:01:33 +0700

    Change division.c

--- shared_lib_dl/src/division.c
+++ shared_lib_dl/src/division.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+
 #include "calculator.h"
 
-int calculator(int a, int b) { return a / b; }
+int calculator(int a, int b) {
+  if (b != 0) {
+    return a / b;
+  }
+  printf(
+      "На ноль делить нельзя!\n"
+      "Пусть результат будет равен 0, чтобы прога не ломалась)))\n");
+  return 0;
+}
