d58f67517ed2bd1ea0cb7cb002fb28e81c0b108b
Andrey525 <ghost5run@yandex.ru>
2022-02-23 13:29:05 +0700

    Revise fork

--- fork/main.c
+++ fork/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-  pid_t pid;
-  int status;
+    pid_t pid;
+    int status;
 
-  pid = fork();
-  if (pid == 0) {  // доча
-    printf("Доча: pid = %d, parent_pid = %d\n", getpid(), getppid());
-  } else {  // папа
-    printf("Папа: pid = %d, parent_pid = %d\n", getpid(), getppid());
-    wait(&status);
-  }
-  return 0;
+    pid = fork();
+    if (pid == 0) { // доча
+        printf("Доча: pid = %d, parent_pid = %d\n", getpid(), getppid());
+        exit(EXIT_SUCCESS);
+    } else { // папа
+        printf("Папа: pid = %d, parent_pid = %d\n", getpid(), getppid());
+        wait(&status);
+    }
+    return 0;
 }
