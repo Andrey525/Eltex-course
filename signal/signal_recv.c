fdd32bc28ee37848c06ba598ffc549917e1ab2e3
Andrey525 <ghost5run@yandex.ru>
2022-03-18 21:05:56 +0700

    Add sigprocmask, sigsuspend

--- signal/signal_recv.c
+++ signal/signal_recv.c
@@ -13,6 +13,11 @@ void handler(int signum) {
 }
 
 int main() {
+    sigset_t set, old_set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGUSR1);
+    sigaddset(&set, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &set, &old_set);
     printf("pid = %d\n", getpid());
 
 #if FLAG == 0
@@ -29,7 +34,17 @@ int main() {
 
 #endif
 
-    while (1)
-        ;
+    while (1) {
+        // Sigsuspend временно изменяет значение маски блокировки сигналов
+        // процесса на указанное old_set
+        // и то есть SIGUSR1 и SIGUSR2 сейчас разблокированы
+        sigsuspend(&old_set);
+        // при sigsuspend возврате он сбрасывает маску сигнала процесса до
+        // исходного значения, в этом случае
+        // SIGUSR1 и SIGUSR2 сигналы снова блокируются.
+
+        //  sigprocmas явно разблокирует сигналы из множества
+        sigprocmask(SIG_UNBLOCK, &set, NULL);
+    }
     return 0;
 }
